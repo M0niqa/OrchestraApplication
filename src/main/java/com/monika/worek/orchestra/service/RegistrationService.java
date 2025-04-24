71612e2cbe3f84d6b39378ea35d0217519074392
m0niqa <monikwor91@gmail.com>
2025-04-24 07:50:02 +0200

    Added files

---
+++ src/main/java/com/monika/worek/orchestra/service/RegistrationService.java
@@ -0,0 +1,53 @@
+package com.monika.worek.orchestra.service;
+
+import com.monika.worek.orchestra.dto.MusicianRegisterDTO;
+import com.monika.worek.orchestra.model.Musician;
+import com.monika.worek.orchestra.model.Role;
+import com.monika.worek.orchestra.model.Token;
+import com.monika.worek.orchestra.model.UserRole;
+import com.monika.worek.orchestra.repository.UserRepository;
+import com.monika.worek.orchestra.repository.UserRoleRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RegistrationService {
+
+    private final EmailService emailService;
+    private final UserRepository userRepository;
+    private final UserRoleRepository userRoleRepository;
+    private final TokenService tokenService;
+
+    public RegistrationService(EmailService emailService, UserRepository userRepository, UserRoleRepository userRoleRepository, TokenService tokenService) {
+        this.emailService = emailService;
+        this.userRepository = userRepository;
+        this.userRoleRepository = userRoleRepository;
+        this.tokenService = tokenService;
+    }
+
+    public void sendLink(String email) {
+        Token token = tokenService.createToken(email);
+        String setupLink = "http://localhost:8080/reset-password?token=" + token.getToken();
+        emailService.sendEmail(email, "Complete Your Registration", "Click here to set your password and complete registration: " + setupLink);
+    }
+
+    @Transactional
+    public void createMusician(MusicianRegisterDTO musicianRegisterDTO) {
+        if (userRepository.findByEmail(musicianRegisterDTO.getEmail()).isPresent()) {
+            throw new IllegalArgumentException("Musician already exists.");
+        }
+
+        Musician musician = new Musician();
+        musician.setFirstName(musicianRegisterDTO.getFirstName());
+        musician.setLastName(musicianRegisterDTO.getLastName());
+        musician.setEmail(musicianRegisterDTO.getEmail());
+
+        UserRole musicianRole = userRoleRepository.findByName(Role.MUSICIAN)
+                .orElseThrow(() -> new IllegalStateException("Role MUSICIAN not found"));
+
+        musician.getRoles().add(musicianRole);
+        userRepository.save(musician);
+    }
+
+}
+
---
+++ src/main/java/com/monika/worek/orchestra/service/TokenService.java
@@ -0,0 +1,48 @@
+package com.monika.worek.orchestra.service;
+
+import com.monika.worek.orchestra.model.Token;
+import com.monika.worek.orchestra.repository.TokenRepository;
+import jakarta.transaction.Transactional;
+import lombok.Getter;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Service
+public class TokenService {
+
+    private final TokenRepository tokenRepository;
+
+    public TokenService(TokenRepository tokenRepository) {
+        this.tokenRepository = tokenRepository;
+    }
+
+    public Token createToken(String email) {
+        String randomUUID = UUID.randomUUID().toString();
+        Token token = new Token();
+        token.setToken(randomUUID);
+        token.setEmail(email);
+        token.setExpiryDate(LocalDateTime.now().plusMinutes(5));
+
+        tokenRepository.save(token);
+        return token;
+    }
+
+    public String getEmailForToken(String token) {
+        Token resetToken = tokenRepository.findByToken(token)
+                .orElse(null);
+
+        if (resetToken == null || resetToken.getExpiryDate().isBefore(LocalDateTime.now())) {
+            return null;
+        }
+        return resetToken.getEmail();
+    }
+
+    @Transactional
+    public void invalidateToken(String token) {
+        tokenRepository.deleteByToken(token);
+    }
+
+}
