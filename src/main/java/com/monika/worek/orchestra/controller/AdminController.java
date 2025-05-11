6d376a773a1c4a3396c00a64777b0c6121822346
m0niqa <monikwor91@gmail.com>
2025-05-11 19:02:11 +0200

    Updated chat unreads for admin

--- src/main/java/com/monika/worek/orchestra/controller/AdminController.java
+++ src/main/java/com/monika/worek/orchestra/controller/AdminController.java
@@ -2,8 +2,10 @@ package com.monika.worek.orchestra.controller;
 
 import com.monika.worek.orchestra.dto.ProjectDTO;
 import com.monika.worek.orchestra.dto.UserBasicDTO;
+import com.monika.worek.orchestra.service.ChatService;
 import com.monika.worek.orchestra.service.ProjectService;
 import com.monika.worek.orchestra.service.UserService;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,14 +20,22 @@ public class AdminController {
 
     private final ProjectService projectService;
     private final UserService userService;
+    private final ChatService chatService;
 
-    public AdminController(ProjectService projectService, UserService userService) {
+    public AdminController(ProjectService projectService, UserService userService, ChatService chatService) {
         this.projectService = projectService;
         this.userService = userService;
+        this.chatService = chatService;
     }
 
     @GetMapping("/adminPage")
-    public String showAdminPage(Model model) {
+    public String showAdminPage(Model model, Authentication authentication) {
+        String currentEmail = authentication.getName();
+        UserBasicDTO userBasicDTO = userService.getUserBasicDtoByEmail(currentEmail);
+        long userId = userBasicDTO.getId();
+        Boolean unreads = chatService.areUnreadMessages(userBasicDTO.getId());
+        model.addAttribute("unreads", unreads);
+        model.addAttribute("userId", userId);
         model.addAttribute("ongoingProjects", projectService.getOngoingProjects());
         model.addAttribute("futureProjects", projectService.getFutureProjects());
         return "/admin/admin-main-page";
