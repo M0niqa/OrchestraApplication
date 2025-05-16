c66e1ac5f97aa5e720a408c143de5dcb3416a49b
m0niqa <monikwor91@gmail.com>
2025-05-16 16:52:43 +0200

    updated chat, working

--- src/main/resources/static/js/chatBadge.js
+++ src/main/resources/static/js/chatBadge.js
@@ -2,14 +2,13 @@ document.addEventListener("DOMContentLoaded", function () {
     const userDataEl = document.getElementById("userData");
     if (!userDataEl) return;
 
-    const userId = userDataEl.dataset.userId;
     const socket = new SockJS('/websocket');
     const stompClient = Stomp.over(socket);
 
     const chatBadge = document.getElementById('chatBadge');
 
     stompClient.connect({}, function () {
-        stompClient.subscribe('/topic/messages/' + userId, function () {
+        stompClient.subscribe('/user/queue/messages', function () {
             if (chatBadge) {
                 chatBadge.classList.remove('d-none');
             }
